2c585d395619c51233e79ba7aca9cd3a4018c9eb
Sasha <skalitsenia@mail.ru>
2023-05-28 23:18:03 +0300

    Add Post and Test and User  c new students

--- src/students/students.tsx
+++ src/students/students.tsx
@@ -1,5 +1,12 @@
-export  const  Students = {
-    id: 1,
-    name : "Masha",
-    age : 21
-}
+export const Students = [
+    {id: 1, name: 'Hope', age: 21},
+    {id: 2, name: 'Masha', age: 23},
+    {id: 3, name: 'Sveta', age: 22},
+    {id: 4, name: 'Alina', age: 25},
+    {id: 5, name: 'Dasha', age: 29},
+    {id: 6, name: 'Sasha', age: 19},
+    {id: 7, name: 'Alex', age: 20},
+    {id: 8, name: 'Mikle', age: 26},
+    {id: 9, name: 'Marina', age: 27},
+    {id: 10, name: 'Karina', age: 28}
+]
