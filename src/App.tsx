7842d4fa625dc64e20a0a783dbef4c72b185a86a
Sasha <skalitsenia@mail.ru>
2023-05-29 15:57:49 +0300

    Add Post and Test and User  c new students

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,24 @@ import {NewComponents} from "./students/NewComponents";
 import {Students} from "./students/students";
 import {TopCars} from "./Cars/topCars";
 import {topCars} from "./Cars/Cars";
-import {Button, ButtonFour, ButtonTree, ButtonTwo} from "./button/button";
+import {Button, ButtonFour, ButtonTree, ButtonTwo, ButtonFive} from "./button/button";
 
 function App() {
+    const Button1Foo = (firstName : string)=> {
+        console.log(firstName)
+    }
+    const Button2Foo = (firstName : string)=> {
+        console.log(firstName)
+    }
+    const Button3Foo = (firstName : string)=> {
+        console.log(firstName)
+    }
+    const Button4Foo = (firstName : string)=> {
+        console.log(firstName)
+    }
+    const ButtonStop =()=> {
+        console.log(("Button-Stop :)"))
+    }
     return (
         <div>
             <Header title={'NEW BODY'}/>
@@ -17,10 +32,11 @@ function App() {
             <Foter titleForFoter={'NEW FOTER'}/>
             <NewComponents students={Students}/>
             <TopCars cars={topCars}/>
-            <Button/>
-            <ButtonTwo/>
-            <ButtonTree/>
-            <ButtonFour/>
+            <Button name={"MyYouTubeChanel-1"} callBack={() =>Button1Foo("Sveta")}/>
+            <ButtonTwo name={"MyYouTubeChanel-2"} callBack={() =>Button2Foo("Masha")}/>
+            <ButtonTree name={"Example-Click 1"} callBack={() =>Button3Foo("Dasha")}/>
+            <ButtonFour name={"Example-Click 1"} callBack={() =>Button4Foo("Sasha")}/>
+            <ButtonFive name={"Click-stop"} callBack={ButtonStop}/>
         </div>
     );
 }
--- src/button/button.tsx
+++ src/button/button.tsx
@@ -1,47 +1,66 @@
 import b from "./button.module.css"
 
-export const Button = () => {
+type ButtonType = {
+    name: string
+    callBack: () => void
+}
+export const Button = (props: ButtonType) => {
     const ClickOne = (name: string) => {
+        props.callBack()
         console.log(name)
     }
     return (
         <div className={b.button}>
-            <button onClick={()=> ClickOne("Sasha") }> MyYouTubeChanel-1 </button>
+            <button onClick={() => ClickOne("Sasha")}> {props.name}</button>
         </div>
     )
 }
 
-export const ButtonTwo = () => {
+export const ButtonTwo = (props: ButtonType) => {
     const ClickTwo = (name: string) => {
+        props.callBack()
         console.log(name)
     }
     return (
         <div className={b.button}>
-            <button onClick={(event)=> ClickTwo("Dasha")} >MyYouTubeChanel-2</button>
+            <button onClick={(event) => ClickTwo("Dasha")}>{props.name}</button>
         </div>
     )
 }
-export const ButtonTree = ()=> {
+export const ButtonTree = (props: ButtonType) => {
     const foo1 = () => {
-        console.log("no things or 100200" )
+        props.callBack()
     }
-    return(
+    return (
         <div className={b.button}>
-            <button onClick={foo1}>Example-Click 1</button>
+            <button onClick={foo1}>{props.name}</button>
         </div>
     )
 }
 
-export  const ButtonFour = ()=> {
-const foo2 = (num : number)=> {
-    console.log(num)
+export const ButtonFour = (props: ButtonType) => {
+    const foo2 = (num: number) => {
+        props.callBack()
+        console.log(num)
+    }
+
+
+    return (
+        <div className={b.button}>
+            <button onClick={(event) => foo2(100200)}>{props.name}</button>
+
+        </div>
+    )
 }
 
 
-    return(
+export const ButtonFive = (props: ButtonType) => {
+    const foo3 = () => {
+        props.callBack()
+    }
+    return (
         <div className={b.button}>
-            <button onClick={(event)=> foo2(100200)}>Example-Click 2</button>
-
+            <button onClick={foo3}>{props.name}</button>
         </div>
     )
 }
