54e7be515bce54fd0dc899f2a1e56c0ed3c61f59
Sasha <skalitsenia@mail.ru>
2023-06-06 05:04:10 +0300

    Without clues

--- src/input/button.tsx
+++ src/input/button.tsx
@@ -1,23 +1,25 @@
 import {ChangeEvent, useState} from "react";
 
-
-type CreatMessage = {
-    creatMessage: (title: string)=> void
+type creatMessageType= {
+    creatMessage: (title: string) => void
 }
 
-export const Button = (props : CreatMessage) => {
-    let [title, seTitle] = useState('');
-    const OnChangeHandler= (event: ChangeEvent<HTMLInputElement>)=>{
-        seTitle(event.currentTarget.value)
+export const Button = (props: creatMessageType) => {
+
+    const [title, setTitle] = useState('')
+    console.log(title)
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
     }
-    const onClickButtonHandler=()=>{
-        props.creatMessage(title);
-seTitle('')
+    const onClickHandler = () => {
+        props.creatMessage(title)
+        setTitle('');
     }
 
     return (<div>
-            <input onChange={OnChangeHandler} value={title}/>
-            <button onClick={onClickButtonHandler}>Click</button>
+            <input onChange={onChangeHandler} value={title}/>
+            <button onClick={onClickHandler}>Click</button>
         </div>
     )
 }
