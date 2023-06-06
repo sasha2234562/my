5b45100e40620bbe1590396147b6aae5b43336f5
Sasha <skalitsenia@mail.ru>
2023-06-06 04:50:35 +0300

    Add Post and Test and User input-button

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,26 @@ import {Button} from "./input/button";
 
 
 function App() {
-    // let [message, setMessege] = useState([
-    //     {message: 'Message1'},
-    //     {message: 'Message2'},
-    //     {message: 'Message11'}
-    // ])
-return(
-    <div>
-        <Button/>
-    </div>
-)
+    let [message, setMessege] = useState([
+        {message: 'Message1'},
+        {message: 'Message2'},
+        {message: 'Message11'}
+    ])
+
+    const creatMessange = (title: string) => {
+        console.log("Masha");
+        console.log(title);
+        let newMessage = {message: title}
+        setMessege([newMessage, ...message])
+    }
+    return (
+        <div>
+            <Button creatMessage={creatMessange}/>
+            <div>
+                {message.map((item, index) => <li key={index}>{item.message}</li>)}
+            </div>
+        </div>
+    )
 }
 
 export default App;
