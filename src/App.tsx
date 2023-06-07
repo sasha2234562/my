efad9748ce9154d4e3b0b657e8d544f79115fb14
Sasha <skalitsenia@mail.ru>
2023-06-07 04:19:32 +0300

    repeat

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,17 @@ function App() {
         {message: 'Message2'},
         {message: 'Message11'}
     ])
-
+    const [title, setTitle] = useState('')
+console.log(title)
+    const  callBackButtonHandler=()=>{
+        console.log("Nastya");
+        const neMessage = {message : title}
+        setMessege([neMessage, ...message])
+    }
     return (
         <div>
-            <Button name={'Click'} collBack={()=>{}}/>
-            <FullInput/>
+            <Button name={'Click'} collBack={callBackButtonHandler}/>
+            <FullInput setTitle={setTitle} title={title}/>
             <div>
                 {message.map((item, index) => <li key={index}>{item.message}</li>)}
             </div>
