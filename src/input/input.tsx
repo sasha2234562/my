38c2cabe7a5e105980fd0c8ce57ecfc13243bd66
Sasha <skalitsenia@mail.ru>
2023-06-07 03:47:38 +0300

    repeat

--- src/input/input.tsx
+++ src/input/input.tsx
@@ -1,21 +1,9 @@
 import React, {ChangeEvent} from "react";
 
-type inputType = {
-    setTitle: (title: string) => void,
-    title: string
-}
 
 
-export const FullInput = (props: inputType) => {
-const onChangeHandler= (event: ChangeEvent<HTMLInputElement>)=> {
-props.setTitle(event.currentTarget.value)
-}
+export const FullInput = () => {
 
-return(
-    <div>
-        <input title={props.title} onChange={onChangeHandler}/>
-    </div>
-)
 }
 
 
