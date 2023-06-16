7205c17a79c6bd53f73a16dcdb5031cad62118b0
Sasha <skalitsenia@mail.ru>
2023-06-16 23:30:19 +0300

    click++

---
+++ src/example/example.tsx
@@ -0,0 +1,43 @@
+import {Copu} from "./Copu";
+import {useState} from "react";
+
+export type moneyType = {
+    banknots: string,
+    value: number,
+    number: string
+}
+
+export function Example() {
+
+
+    const [money, setMoney] = useState<Array<moneyType>>([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+    const [filter, setFilter] = useState('all');
+    const currentMoney = money;
+    if (filter === 'Dollars') {
+        currentMoney = money.filter((item) => item.banknots === 'Dollars')
+    }
+    if (filter === 'RUBLS') {
+        currentMoney = money.filter((item) => item.banknots === 'RUBLS')
+    }
+    return currentMoney
+
+    function filterMoney(value: string) {
+        console.log(value);
+        setFilter(value)
+    }
+
+    return (
+        <div>
+            <Copu money={money} filterMoney={filterMoney}/>
+        </div>
+    )
+}
