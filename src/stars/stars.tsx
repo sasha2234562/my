e3e749928225bff40013fb0287e7706002afd80c
Sasha <skalitsenia@mail.ru>
2023-06-21 22:44:14 +0300

    work night

--- src/stars/stars.tsx
+++ src/stars/stars.tsx
@@ -1,16 +1,21 @@
 
-type propsType = {
-    selected  : boolean
-}
-export function Stars(props : propsType) {
 
-    return(
+type propsType = {
+    selected: boolean
+    click: (star: number) => void
+    // steValue: (n: number)=>void
+    value: number
+}
+
+export function Stars(props: propsType) {
+
+    function onClickHandler() {
+        props.click(props.value)
+    }
+
+    return (
         <div>
-            {props.selected ? <span><b>star</b></span> : <span>star</span>}
-            {/*<span>star</span>*/}
-            {/*<span>star</span>*/}
-            {/*<span>star</span>*/}
-            {/*<span>star</span>*/}
+            {props.selected ? <span onClick={onClickHandler}><b>star</b></span> : <span onClick={onClickHandler}>star</span>}
         </div>
     )
 }
